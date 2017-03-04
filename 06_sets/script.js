636075be9796e739e9d51a3d604158982bb1bf38
besseddrest <hcabalic@gmail.com>
2017-03-04 13:48:02 -0800

    implement values, union, intersection, difference, subset

--- 06_sets/script.js
+++ 06_sets/script.js
@@ -1,8 +1,12 @@
 function Set() {
   let items = {};
+
+  // checks if value exists
   this.has = (value) => {
     return items.hasOwnProperty(value);
   };
+
+  // adds new value if doesn't exist
   this.add = (value) => {
     if (!this.has(value)) {
       items[value] = value;
@@ -10,6 +14,8 @@ function Set() {
     }
     return false;
   };
+
+  // deletes value if exists
   this.remove = (value) => {
     if (this.has(value)) {
       delete items[value];
@@ -17,16 +23,92 @@ function Set() {
     }
     return false;
   };
+
+  // clears values
   this.clear = () => {
     items = {};
   };
+
+  // gets number of values
   this.length = () => {
     return Object.keys(items).length;
+  };
+
+  // returns values in an array
+  this.values = () => {
+    let values = [];
+    for (let i = 0, tempItems = Object.keys(items); i < tempItems.length; i++) {
+      values.push(tempItems[i]);
+    }
+    return values;
+  }
+
+  // returns a new Set containing values from set A and set B
+  this.union = (otherSet) => {
+    let union = new Set();
+
+    let values = this.values();
+    for (let i = 0; i < values.length; i++){
+      union.add(values[i]);
+    }
+
+    values = otherSet.values();
+    for (let i = 0; i < values.length; i++){
+      union.add(values[i]);
+    }
+
+    return union;
+  }
+
+  // returns a new set containing values shared by set A and set B
+  this.intersection = (otherSet) => {
+    let intersection = new Set();
+
+    let values = this.values();
+    for (let i = 0; i < values.length; i++) {
+      if (otherSet.has(values[i]))
+        intersection.add(values[i]);
+    }
+
+    return intersection;
+  }
+
+  // returns a new set containing values not shared by set A and set B
+  this.difference = (otherSet) => {
+    let difference = new Set();
+
+    let values = this.values();
+    for (let i = 0; i < values.length; i++) {
+      if (!otherSet.has(values[i]))
+        difference.add(values[i]);
+    }
+
+    return difference;
+  }
+
+  // returns true if all the values in set A exist in set B
+  this.subset = (otherSet) => {
+    let values = this.values();
+    for (let i = 0; i < values.length; i++) {
+      if (!otherSet.has(values[i]))
+        return false;
+    }
+
+    return true;
   }
 }
 
-const newSet = new Set();
-newSet.add('Harold'); // false
-newSet.add('Sarah'); // false
-newSet.add('Brian'); // false
-console.log(newSet.length());
+const firstSet = new Set();
+firstSet.add('Mom');
+firstSet.add('Dad');
+console.log(firstSet.values());
+
+const secondSet = new Set();
+secondSet.add('Harold');
+secondSet.add('Sarah');
+secondSet.add('Brian');
+secondSet.add('Mom');
+secondSet.add('Dad');
+
+const subset = firstSet.subset(secondSet);
+console.log(subset);
